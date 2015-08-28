d4d0954f2762232cae851041782e0caf9377fa72
gagin <alexander@gagin.ru>
2015-08-28 01:01:40 -0700

    0.78947 like this

--- titanic.R
+++ titanic.R
@@ -1,5 +1,5 @@
 # setwd("kaggle-titanic/")
-
+# 0.78947
 train<-read.csv("train.csv")
 test<-read.csv("test.csv")
 library(dplyr)
@@ -62,36 +62,80 @@ cleanup<-function(vect){
                 else if(x>0) 1
                 else 0})}
 
+qualify<-function(real,guess){
+        check<-table(real,guess)
+        if(sum(dim(check)==c(2,2))!=2) percentage<-0
+        else{
+                good.ones<-check[1,1]+check[2,2]
+                bad.ones<-check[1,2]+check[2,1]
+                #paste0(as.character()),'%')
+                percentage<-round(100*good.ones/(good.ones+bad.ones))
+        }#if(is.na(percentage)) percentage<-0
+        return(percentage)
+}
+
 # In internal tests, split train set to train/test part and check how
 # the selected algorithm works
 ## BEGIN internal tests
 if(FALSE){
-        qualify<-function(real,guess){
-                check<-table(real,guess)
-                good.ones<-check[1,1]+check[2,2]
-                bad.ones<-check[1,2]+check[2,1]
-                paste0(as.character(round(100*good.ones/(good.ones+bad.ones))),'%')
-        }
         
-        nr<-dim(bins)[1] # number of observations
-        share<-0.8 # this is our 80% parameter
-        set.seed(seed)
-        trainset<-sample.int(nr,round(share*nr))
         
-        trainers<-bins[trainset,]
-        testers<-bins[-trainset,]
+        #nr<-dim(bins)[1] # number of observations
+        #share<-0.8 # this is our 80% parameter
+        #set.seed(seed)
+        #trainset<-sample.int(nr,round(share*nr))
+        
+        #trainers<-bins[trainset,]
+        #testers<-bins[-trainset,]
+        trainers<-bins
         
         nfeat<-dim(bins)[2] 
         
-        n.testers<-bins.nn(trainers,rep=3,hidden=c(4),threshold=0.25)
+        #n.testers<-bins.nn(trainers,rep=3,hidden=c(4),threshold=0.25)
         
-        res.testers<-neuralnet::compute(n.testers,testers[,3:nfeat])
-        qualify(cleanup(round(res.testers$net.result)),testers$Survived)
+        #res.testers<-neuralnet::compute(n.testers,testers[,3:nfeat])
+        #qualify(cleanup(round(res.testers$net.result)),testers$Survived)
+
+        mult<-list()
+        eff<-list()
+        tries=50
+        for(i in 1:tries){
+                cat("Iteration #",i,"/",tries,"\n")
+                set.seed(i)
+                r<- 3#as.integer(runif(1,5,10))
+                h<- 5#as.integer(runif(1,5,10))
+                t<- 0.2#as.integer(runif(1,5,10))
+                nr1<-dim(trainers)[1]
+                ttrainset<-sample.int(nr1,round(0.9*nr1))
+                ttrainers<-trainers[ttrainset,]
+                ttesters<-trainers[-ttrainset,]
+                mult[[i]]<-bins.nn(ttrainers,rep=r,hidden=h,threshold=t)
+                
+                res<-neuralnet::compute(mult[[i]],ttesters[,3:nfeat])
+                eff[[i]]<-qualify(cleanup(round(res$net.result)),
+                                  ttesters$Survived)
+                print(eff[[i]])
+        }
+        
+        pult<-matrix(NA, nrow=dim(bins.test)[1])
+        alltries<-1:tries
+        mineff<-85##########################
+        goodtries<-alltries[unlist(eff)>mineff]
+        for(i  in goodtries){
+                res<-neuralnet::compute(mult[[i]],bins.test[,2:nfeat.test])#testers[,3:nfeat])
+                pult<-cbind(pult,cleanup(round(res$net.result)))                           
+        }
+        pult<-dplyr::select(as.data.frame(pult),-V1) # drop NA column
+        predi<-rowSums(pult)
+        cu<-mean(predi[predi!=0]) ###############
+        #cu<-0.5*max(predi)
+        ppredi<-sapply(predi,function(x)if(x>cu) 1 else 0)
+        #qualify(ppredi,testers$Survived)
 }
 ## END internal tests
         
 # Train with full train set
-n.full<-bins.nn(bins,rep=5,hidden=c(4),threshold=0.25)
+#n.full<-bins.nn(bins,rep=5,hidden=c(4),threshold=0.25)
 
 ##### Now test data
 
@@ -119,10 +163,54 @@ bins.test<-ts1 %>% # piped functions follow
 
 nfeat.test<-dim(bins.test)[2] 
 
-res<-neuralnet::compute(n.full,bins.test[,2:nfeat.test])
-upload<-cleanup(round(res$net.result))
+#res<-neuralnet::compute(n.full,bins.test[,2:nfeat.test])
+#upload<-cleanup(round(res$net.result))
 
+trainers<-bins
 
+nfeat<-dim(bins)[2] 
+
+#n.testers<-bins.nn(trainers,rep=3,hidden=c(4),threshold=0.25)
+
+#res.testers<-neuralnet::compute(n.testers,testers[,3:nfeat])
+#qualify(cleanup(round(res.testers$net.result)),testers$Survived)
+
+mult<-list()
+eff<-list()
+tries=5
+for(i in 1:tries){
+        cat("Iteration #",i,"/",tries,"\n")
+        set.seed(i)
+        r<- 1#as.integer(runif(1,5,10))
+        h<- 5#as.integer(runif(1,5,10))
+        t<- 1#as.integer(runif(1,5,10))
+        nr1<-dim(trainers)[1]
+        ttrainset<-sample.int(nr1,round(0.9*nr1))
+        ttrainers<-trainers[ttrainset,]
+        ttesters<-trainers[-ttrainset,]
+        mult[[i]]<-bins.nn(ttrainers,rep=r,hidden=h,threshold=t)
+        
+        res<-neuralnet::compute(mult[[i]],ttesters[,3:nfeat])
+        eff[[i]]<-qualify(cleanup(round(res$net.result)),
+                          ttesters$Survived)
+        print(eff[[i]])
+}
+
+pult<-matrix(NA, nrow=dim(bins.test)[1])
+alltries<-1:tries
+mineff<-85##########################
+goodtries<-alltries[unlist(eff)>mineff]
+for(i  in goodtries){
+        res<-neuralnet::compute(mult[[i]],bins.test[,2:nfeat.test])#testers[,3:nfeat])
+        pult<-cbind(pult,cleanup(round(res$net.result)))                           
+}
+pult<-dplyr::select(as.data.frame(pult),-V1) # drop NA column
+predi<-rowSums(pult)
+cu<-mean(predi[predi!=0]) ###############
+#cu<-0.5*max(predi)
+ppredi<-sapply(predi,function(x)if(x>cu) 1 else 0)
+
+upload<-ppredi
 names(upload)<-c("Survived")
 upload1<-data.frame(cbind(bins.test$PassengerId,upload))
 names(upload1)<-c("PassengerId","Survived")
